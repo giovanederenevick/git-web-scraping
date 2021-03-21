d735f91f0e8ebf0e2ee213ba6ab653c287d7935d
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-03-21 10:52:13 -0300

    Project created

---
+++ src/main/java/com/giovanederenevickfilho/gws/GitWebScrapingApplication.java
@@ -0,0 +1,13 @@
+package com.giovanederenevickfilho.gws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWebScrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWebScrapingApplication.class, args);
+	}
+
+}
